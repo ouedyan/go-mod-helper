3fafd75163508c52d7b6225a53c33611dfa2ab2d
GitHub Action <action@github.com>
2023-01-19 17:11:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/ouedyan/gomodhelper/MyBundle.kt
+++ src/main/kotlin/com/github/ouedyan/gomodhelper/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ouedyan.gomodhelper
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ouedyan/gomodhelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ouedyan/gomodhelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ouedyan.gomodhelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ouedyan.gomodhelper.MyBundle
 
 class MyApplicationService {
 
